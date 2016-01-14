7c9858c57235c7f473148b4bd276f04c3b46273f
xiaoke <nygxk88@163.com>
2016-01-15 00:09:44 +0800

    处理文本

--- io.py
+++ io.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-f = open('test.txt', 'r')
-
-print f.read()
-
-f.close()
-
+import re
+file = open('D:\\bank\\export_30000.csv', 'r')
 f = open('test.txt', 'w')
-f.write('Hello, world!')
+# while 1:
+    # line = file.readline()
+    # if not line:
+        # break
+    # pass # do something
+	# print line
+pattern = re.compile(r'´óÌü')
+for eachline in file:
+    match=pattern.search(eachline)
+    if match:
+    #print eachline,;
+	    f.write(eachline)
+file.close()
+
 f.close()
 
